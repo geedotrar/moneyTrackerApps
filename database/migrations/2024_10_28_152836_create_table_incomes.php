4ac12f6fe44b67c820d0ce7ec4e389f6c10c04cf
geedotrar <geedotrar@gmail.com>
2024-10-29 22:27:10 +0700

    feat/add new migrations

---
+++ database/migrations/2024_10_28_152836_create_table_incomes.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('table_incomes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->decimal('amount', 15, 2);
+            $table->string('source');
+            $table->text('description')->nullable();
+            $table->date('date');
+            $table->unsignedBigInteger('payment_method');
+            $table->string('payment_method_name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table_incomes');
+    }
+};
